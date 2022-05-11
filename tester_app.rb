4d06c4202668b0b8632f07d4f61ba65c63a47937
Theo <theo@thetaphoto.co.uk>
2022-05-11 14:04:49 +0100

    Tester app complete

--- tester_app.rb
+++ tester_app.rb
@@ -14,7 +14,11 @@ menu_formatter = MenuFormatter.new(menu)
 
 user_selection = []
 while true
+  puts
+  puts 'MENU'
+  puts '----'
   puts menu_formatter.format
+  puts
   print 'Choose dish number or N for next step: '
   user_response = gets.chomp
   break if user_response.downcase == 'n'
@@ -25,8 +29,39 @@ cart = Cart.new
 user_selection.map{|item| cart.add_item(item)}
 
 cart_formatter = CartFormatter.new(cart)
+puts 'Your cart contents:'
+puts cart_formatter.format
+puts
+puts 'Please enter your details:'
+puts '--------------------------'
+print 'What is your name? '
+full_name = gets.chomp
+print 'Where do you live? '
+address = gets.chomp
+print 'What is your mobile number? '
+mobile = gets.chomp
+customer = Customer.new(full_name, address, mobile)
+order = Order.new(customer, cart)
+order_formatter = OrderFormatter.new(order)
+
 puts cart_formatter.format
 
+puts
+puts 'Your cart contents:'
+puts '-------------------'
+print 'Please confirm your order (y/n): '
+confirm_order = gets.chomp
 
-# unless submitted
-# end
+if confirm_order.downcase == 'y'
+  order.submit
+  puts
+  puts 'Your order details:'
+  puts '-------------------'
+  puts order_formatter.format
+  delivery_text_formatter = DeliveryTextFormatter.new(order)
+  text_sender = TextSender.new(delivery_text_formatter.format, customer.mobile)
+  text_sender.send
+else
+  puts 'Order cancelled!'
+  puts 'Thank you!'
+end
