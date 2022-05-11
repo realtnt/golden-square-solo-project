0c6801d40b132a9790d7b3c40a2c9ca02ed9188e
Theo <theo@thetaphoto.co.uk>
2022-05-11 07:54:06 +0100

    Implemented the TextService interface

--- spec/text_service_spec.rb
+++ spec/text_service_spec.rb
@@ -1,8 +1,10 @@
 require 'text_service'
 
 RSpec.describe TextService do
-  context '' do
-    it '' do
+  context 'When calling send_text' do
+    it 'it should fail: Method not implemented' do
+      service = TextService.new
+      expect{service.send_text}.to raise_error('Method not implemented')
     end
   end
 end
